379ff5f6147e32422f091f8b3379c49b5b5736d3
Mor992 <152429764+Mor992@users.noreply.github.com>
2025-11-25 15:30:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ import cv2
 
 # Optional: allow downloading the model from Google Drive using gdown
 # If you prefer to place the model file manually, put it in the app folder and set MODEL_PATH accordingly.
-GDRIVE_FILE_ID = "1MA6mvotqb_RkswHivYjkZXbba0HVWpYN"
-DEFAULT_MODEL_FILENAME = "resnet_model.keras"  # change if your model is a SavedModel folder
+GDRIVE_FILE_ID = "1CpmFwks2GdR9asokDAs7oDQs4GoeakSF"
+DEFAULT_MODEL_FILENAME = "1resnet_model.h5"  # change if your model is a SavedModel folder
 
 st.set_page_config(page_title="Skin / Lesion Classifier", layout="wide")
 
